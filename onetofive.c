394ab7933c142d14046e0974371c3d3bb4bec2fb
Shravani <shravani.mula@thundersoft.com>
2022-02-02 20:34:38 +0530

    this is operators

--- onetofive.c
+++ onetofive.c
@@ -9,9 +9,11 @@ printf("%d\n",1==5==5);
    /*output=>1,0(i++ means first print then increment so)*/
 /*3*/ int x=5;
    printf("%d %d %d\n",x++,x++,x++);
-   /*ouput
-4) int x=2;
+   /*ouput 7 6 5*/
+/*4*/ int x=2;
    printf("%d   ",++x++);
    printf("%d\n",x++);
-5) int k=1;
+   /*output error in thefirst printf at a time increm decreme is not possible*/
+/*5*/int k=1;
    printf("%d==1 is ""%s\n",k,k==1?"TRUE":"FALSE");
+   /*output => 1 is True   => k==1 condition is true so print true*/
